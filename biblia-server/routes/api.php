727207a72968180236dceeff12f02eb086c0fffd
Marcelo Migotto <marcelo.dewiz@gmail.com>
2023-02-23 00:01:29 -0300

    Adicionado Rota de idiomas e versoes na api

--- biblia-server/routes/api.php
+++ biblia-server/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\TestamentoController;
 use App\Http\Controllers\LivroController;
 use App\Http\Controllers\VersiculoController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IdiomaController;
+use App\Http\Controllers\VersaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
@@ -13,7 +15,9 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
     Route::apiResources([
         'testamento' => TestamentoController::class,
         'livro' => LivroController::class,
-        'versiculo' =>VersiculoController::class
+        'versiculo' => VersiculoController::class,
+        'idioma' => IdiomaController::class,
+        'versao' => VersaoController::class
     ]);
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
